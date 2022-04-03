07ab964fc080087cf316b766f1fca4cd83a7ad61
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-03 11:32:50 +0530

    progressbar added

--- nptel.py
+++ nptel.py
@@ -1,7 +1,5 @@
 import json
 import pathlib
-from typing import Optional
-
 import typer as t
 
 BASEDIR = pathlib.Path().absolute()
@@ -12,8 +10,8 @@ app = t.Typer()
 @app.command()
 def startquiz_by_week(
     name: str = t.Option("Hola", prompt="Write Your Name"),
-    week: str = t.Option("1", help="by default 1",prompt="week # : ")
-    
+    week: str = t.Option("1", help="by default 1", prompt="week # : ")
+
 ):
     t.echo(name+" "+week)
 
@@ -39,26 +37,32 @@ def addqus(
     with open(qbank_file+".txt", "r") as file:
         readlines = file.readlines()
 
-    for index in range(noq):
-        qstatement = readlines.pop(0).rstrip("\n")
-        options = readlines[:4]
-        ans = readlines[4]
-        options = [
-            {
-                "value": op.rstrip("\n"),
-                "is_correct": op.rstrip("\n") == ans.rstrip("\n")
-            } for op in options
-        ]
-        for _ in range(no_options+1):
-            readlines.pop(0)
+    import time
+    label = t.style("Convertion", fg=t.colors.GREEN)
+    fchar = t.style(" ", bg=t.colors.GREEN)
+    t.echo("")
+    with t.progressbar(range(noq), fill_char=fchar, label=label) as noqs:
+        for index in noqs:
+            time.sleep(.03)
+            qstatement = readlines.pop(0).rstrip("\n")
+            options = readlines[:4]
+            ans = readlines[4]
+            options = [
+                {
+                    "value": op.rstrip("\n"),
+                    "is_correct": op.rstrip("\n") == ans.rstrip("\n")
+                } for op in options
+            ]
+            for _ in range(no_options+1):
+                readlines.pop(0)
 
-        questions.append(
-            {
-                'qno': index+1,
-                "statement": qstatement,
-                "options": options
-            }
-        )
+            questions.append(
+                {
+                    'qno': index+1,
+                    "statement": qstatement,
+                    "options": options
+                }
+            )
     qbank_json["qustions"] = questions
     json_object = json.dumps(qbank_json, indent=4)
     with open(qbank_file+".json", "w") as f:
