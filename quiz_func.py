96dd4a5dd43a04e3b2a2b4ab714372beedc5ee0e
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-04 11:52:00 +0530

    common functions in 1 file

---
+++ quiz_func.py
@@ -0,0 +1,39 @@
+import time 
+import random as rnd
+import os
+import typer as t
+
+
+def show_result(attempted_qus:int, total_marks:int, no_qus:int)->None:
+    print("--------------------------------------------------------------")
+    t.echo(
+        "| Your Result : " +
+        t.style(f"{total_marks}/{no_qus}", fg=t.colors.BRIGHT_BLUE, bold=True) +
+        "\n| Attempted Qus :" +
+        t.style(f" {attempted_qus} / out of {no_qus}", fg=t.colors.BRIGHT_RED) +
+        "\n| Percentage :" +
+        t.style(f" {total_marks/no_qus:.1%}", fg=t.colors.CYAN) +
+        "\n| Accuracy :" +
+        t.style(f" {total_marks/attempted_qus:.2%}",
+                fg=t.colors.BRIGHT_YELLOW)
+
+    )
+    print("---------------------------------------------------------------")
+
+
+def generate_combo()->list[list[int,int]] :
+    """
+        this generates combination like [1,3],[2,3]
+    """
+    rnd.seed(time.time())   
+    week = rnd.randint(1,9)
+    qus_num = rnd.randint(1,10)
+    combo = [week,qus_num]  
+    return combo   
+
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
